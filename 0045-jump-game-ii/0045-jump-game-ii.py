2ecea430116ef70843162fb519796b0772526fd4
alwinlin8 <53026023+alwinlin8@users.noreply.github.com>
2026-01-11 21:00:08 -0600

    Time: 11 ms (41.43%), Space: 13.1 MB (88.13%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def jump(self, nums):
+        count = 0
+        l = r = 0
+
+        while r < len(nums) - 1:
+            farthest = 0
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+            l = r + 1
+            r = farthest
+            count += 1
+        return count
+
+
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
